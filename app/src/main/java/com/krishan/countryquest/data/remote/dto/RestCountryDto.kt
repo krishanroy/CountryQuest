fb87ef54b5777a25921d07f6cc5616cd00924def
krishanroy <krishanroy@pursuit.org>
2025-11-02 22:27:27 -0500

    dev_krishan: add nav serialization helper to transfer object types to details screen and show details screen

--- app/src/main/java/com/krishan/countryquest/data/remote/dto/RestCountryDto.kt
+++ app/src/main/java/com/krishan/countryquest/data/remote/dto/RestCountryDto.kt
@@ -1,5 +1,8 @@
 package com.krishan.countryquest.data.remote.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class RestCountryDto(
     val name: Name,
     val capital: List<String>,
@@ -8,4 +11,5 @@ data class RestCountryDto(
     val languages: Map<String, String>
 )
 
+@Serializable
 data class Name(val common: String, val official: String)
